855307563fad978f7b9661c51a9b4f3987cb39e8
Arivu-abacies <arivazhagan@abacies.com>
2016-12-22 19:19:11 +0530

    invalid token error form email

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -259,7 +259,7 @@ class SiteController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
             Yii::$app->session->setFlash('success', 'New password was saved.');			
 			$model->sendEmailResetSuccess();			
-			return $this->goHome();
+			 return $this->redirect(['login']);
 			
         }
 
