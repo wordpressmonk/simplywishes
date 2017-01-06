a7a55f6b0b61daeb49eb428fcebc9987dcc8ea3b
dencygb <dency@abacies.com>
2017-01-06 14:46:03 +0530

    general website social icons

--- views/layouts/home-without-image.php
+++ views/layouts/home-without-image.php
@@ -59,22 +59,23 @@ AppAsset::register($this);
 		<hr style="border-color:#1085bf;">
 		<nav>
 		  <ul class="nav nav-pills smp-pills">
-			<li><a href="<?=Yii::$app->homeUrl?>">Home</a></li>
-			<li><a href="<?=Yii::$app->homeUrl?>site/about">About Us</a></li>
-			<li><a href="<?=Yii::$app->homeUrl?>wish/index">Find a Wish</a></li>
-			<li><a href="<?=Yii::$app->homeUrl?>wish/top-wishers">iWish</a></li>
-			<li><a href="<?=Yii::$app->homeUrl?>wish/top-granters">iGrant</a></li>
-			<li><a href="<?=Yii::$app->homeUrl?>happy-stories/index">Happy stories</a></li>
-			<li> 
+			<li id="home"><a href="<?=Yii::$app->homeUrl?>#home">Home</a></li>
+			<li id="abt"><a href="<?=Yii::$app->homeUrl?>site/about#abt">About Us</a></li>
+			<li id="search_wish"><a href="<?=Yii::$app->homeUrl?>wish/index#search_wish">Find a Wish</a></li>
+
+			<li id="top_wishers"><a href="<?=Yii::$app->homeUrl?>wish/top-wishers#top_wishers">iWish</a></li>
+			<li id="i_grant"><a href="<?=Yii::$app->homeUrl?>wish/top-granters#i_grant">iGrant</a></li>
+			<li id="i_wish"><a href="<?=Yii::$app->homeUrl?>happy-stories/index#i_wish">Happy stories</a></li>
+			<li id="edt"> 
 				<?php if(isset(\Yii::$app->user->identity->role) && (\Yii::$app->user->identity->role == 'admin')){ ?>
-					<a href="<?=Yii::$app->homeUrl?>editorial/index">Editorial</a>
+					<a href="<?=Yii::$app->homeUrl?>editorial/index#edt">Editorial</a>
 				<?php } else { ?> 
-					<a href="<?=Yii::$app->homeUrl?>editorial/editorial">Editorial</a>
-				<?php } ?>
-			</li> 
+					<a href="<?=Yii::$app->homeUrl?>editorial/editorial#edt">Editorial</a>
+				<?php } ?>			
+			</li>
 			
 			<?php if(!\Yii::$app->user->isGuest){  ?>
-			<li class="dropdown"><a href="#">Hello,<?php echo substr(\Yii::$app->user->identity->username,0,5)?>..!</a>
+			<li class="dropdown" class="active"><a href="#">Hello,<?php echo substr(\Yii::$app->user->identity->username,0,5)?>..!</a>
 				<ul class="dropdown-menu nav nav-stacked">
 						<li><a href="<?=Yii::$app->homeUrl?>wish/create"><i class="fa fa-clone fa-lg"></i>Add Wish</a></li>
 					<li><a href="<?=Yii::$app->homeUrl?>account/inbox"><i class="fa fa-inbox fa-lg"></i> Inbox</a></li>
@@ -132,7 +133,12 @@ $('ul.nav li.dropdown').hover(function() {
 		}, function() {
 		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 	});
-	
+
+	var hash = window.location.hash;
+	hash = hash.replace('#', '');
+	console.log(hash);
+	$("#"+hash).addClass("active");
+			
 });
 
 
