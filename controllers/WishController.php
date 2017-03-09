790d78bf160de0167857d782a53ff9705fcafefb
Arivu-abacies <arivazhagan@abacies.com>
2017-03-09 19:53:46 +0530

    paypal mode from sandbox mode to live mode

--- controllers/WishController.php
+++ controllers/WishController.php
@@ -407,7 +407,7 @@ class WishController extends Controller
 		// STEP 2: Post IPN data back to paypal to validate
 		//https://www.sandbox.paypal.com/cgi-bin/webscr
 		//$ch = curl_init('https://www.paypal.com/cgi-bin/webscr');
-		$ch = curl_init('https://www.sandbox.paypal.com/cgi-bin/webscr');
+		$ch = curl_init('https://www.paypal.com/cgi-bin/webscr');
 		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
